afc89fe8bc694de85f46a923f458f8838518ba47
xy-IMAW <952137172@qq.com>
2017-11-06 23:44:18 +0800

    1.1

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print("hello world")
+print ('*'*70)
+data=[]
+fr=open('1.txt')
+for line in fr.readlines():
+    line=line.strip()
+    data_line=line.strip("\t")
+    data.append(data_line)
+print(data[0])
+fr.close()    
+f=open('1.txt','a+')
+f.write('i love you!')
+f.close()
